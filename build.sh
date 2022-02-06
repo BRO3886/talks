b1440bd07c01f4533d8e76738fbfe20bf23d86ba
Siddhartha Varma <siddverma1999@gmail.com>
2022-02-06 21:08:38 +0530

    fix: fix build script
    fix: fix build script
    
    fix: fix build script
    
    fix: fix build script
    
    fix: fix build script
    
    fix: fix build script

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
+# npm install -g @slidev/cli
 DIRS=$(ls -d */)
 for DIR in $DIRS; do
   DIRNAME=$(echo $DIR | sed 's:/*$::')
   if find "$DIRNAME/src/package.json"; then
     cd "$DIRNAME/src"
+    yarn install
     echo "Building $DIRNAME"
-    yarn build &
+    yarn build
     cd ../..
   fi
 done
